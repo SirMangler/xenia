949b91d33ce55a535a0212abd60aa63ad5dd8b12
sirmangler <thecatsmangler@gmail.com>
2023-02-17 12:37:41 +0000

    On-Screen Keyboard for XAM UI

--- src/xenia/app/emulator_window.cc
+++ src/xenia/app/emulator_window.cc
@@ -1933,6 +1933,12 @@ void EmulatorWindow::WinRTFrontendDialog::OnDraw(ImGuiIO& io) {
         config::SaveConfig();
       }
 
+      ImGui::Spacing();
+      ImGui::Separator();
+      ImGui::TextWrapped(
+          "Note: Please remember to do your USB filesystem setup, or paths to "
+          "your USB will not work properly!");
+
       ImGui::EndTabItem();
     }
 
