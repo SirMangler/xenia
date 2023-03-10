a62b3fa1a7ecc40512aaed2e815992cb885523e5
Gliniak <Gliniak93@gmail.com>
2023-03-10 13:50:22 +0100

    [UI] Changed default UI font to Tahoma. If it's not available use embedded font
    - Additionally allow user to provide own font size
    - Forced font scaling in notification window to be reasonable size

--- src/xenia/ui/imgui_drawer.h
+++ src/xenia/ui/imgui_drawer.h
@@ -15,6 +15,7 @@
 #include <memory>
 #include <vector>
 
+#include "third_party/imgui/imgui.h"
 #include "xenia/ui/immediate_drawer.h"
 #include "xenia/ui/presenter.h"
 #include "xenia/ui/window.h"
@@ -57,7 +58,7 @@ class ImGuiDrawer : public WindowInputListener, public UIDrawer {
   void SetIgnoreInput(bool ignore);
   bool GetIgnoreInput();
   void Draw(UIDrawContext& ui_draw_context) override;
-  
+
   void ClearDialogs();
 
   ImmediateTexture* GetNotificationIcon(uint8_t user_index) {
@@ -81,6 +82,9 @@ class ImGuiDrawer : public WindowInputListener, public UIDrawer {
  private:
   void Initialize();
   void InitializeFonts();
+  bool LoadCustomFont(ImGuiIO& io, ImFontConfig& font_config, float font_size);
+  bool LoadWindowsFont(ImGuiIO& io, ImFontConfig& font_config, float font_size);
+  bool LoadJapaneseFont(ImGuiIO& io, float font_size);
 
   void SetupNotificationTextures();
   void SetupFontTexture();
@@ -105,7 +109,8 @@ class ImGuiDrawer : public WindowInputListener, public UIDrawer {
   // All currently-attached dialogs that get drawn.
   std::vector<ImGuiDialog*> dialogs_;
 
-  // All queued notifications. Notification at index 0 is currently presented one.
+  // All queued notifications. Notification at index 0 is currently presented
+  // one.
   std::vector<ImGuiNotification*> notifications_;
   // Using an index, not an iterator, because after the erasure, the adjustment
   // must be done for the vector element indices that would be in the iterator
